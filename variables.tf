9748ada26a832939f2cb862a4632edf6e973fc62
nirojasmar <nirojasmar@gmail.com>
2026-01-29 12:36:17 -0500

    Add temp tfvars

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+    type = string
+    description = "region (e.g us-central1)"
+}
+
+variable "environment" {
+    type = string
+    description = "environment (e.g dev, qa, prod, etc)"
+}
