2a88c4bf74b077385f7560eb2c569932bd240e50
nirojasmar <nirojasmar@gmail.com>
2026-02-01 10:50:05 -0500

    Change labels to correct env

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "google_compute_firewall" "allow_rabbitmq" {
 }
 
 resource "google_vpc_access_connector" "connector" {
-  name          = "hobio-${var.environment}-connector-ue1"
+  name          = "hobio-${var.type}-connector-ue1"
   region        = var.region
   ip_cidr_range = "10.8.0.0/28"
   network       = "default"
@@ -175,6 +175,6 @@ resource "google_artifact_registry_repository" "docker_repo" {
   }
 
   labels = {
-    environment = var.environment
+    environment = var.type
   }
 }
